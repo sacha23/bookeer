ee29d882c41557d3dbab2ccd20fb308eb3de0d33
sacha123 <ssai.charan.ece13@itbhu.ac.in>
2016-09-05 17:58:41 +0530

    some handling for iframes in arg_test.py all new functions in booker_new.py

--- arg_test.py
+++ arg_test.py
@@ -2,7 +2,7 @@
 
 # import required modules
 import argparse
-import booker
+import booker_new
 
 # building parser object with required args 
 parser = argparse.ArgumentParser(description="Tool to convert series of webpages into single PDF")
@@ -19,11 +19,30 @@ end = args.end
 print "\n"
 print "main page :"+main+"\nstart page :"+start+"\nend page :"+end
 
-url_list = booker.urllist(main,start,end)
+#url_list = booker.urllist(main,start,end)
 
-print "\nURL's parsed successfully.....\n"
-print "Downloading Webpages and making PDF\n"
+cont = booker_new.get_page(main)
+extr_list = booker_new.extract_url(cont)
+abs_url_list = booker_new.make_abs_url(extr_list,main)
+st_index,end_index = booker_new.find_index(abs_url_list,start,end)
 
-booker.htmltopdf(url_list,main)
-
-print "\nPDF saved successfully as "+main.rsplit('/',2)[1]+".pdf"+" in home directory"
+if not (st_index and end_index):
+	ifr_list = extract_ifr(main)
+	for main in ifr_list:
+		cont = booker_new.get_page(main)
+		extr_list = booker_new.extract_url(cont)
+		abs_url_list = booker_new.make_abs_url(extr_list,main)
+		st_index,end_index = booker_new.find_index(abs_url_list,start,end)
+		if st_index and end_index:
+			break
+if not (st_index):
+	print 'Oops!..Starting URL not found on main page.'
+if not (end_index):
+	print 'Oops!..Ending URL not found on main page.'
+if (st_index and end_index):
+	
+	print "\nURL's parsed successfully.....\n"
+	print st_index
+	print "Downloading Webpages and making PDF\n"
+	#booker_new.htmltopdf(abs_url_list[st_index:end_index+1],main)
+	print "\nPDF saved successfully as "+main.rsplit('/',2)[1]+".pdf"+" in home directory"
--- booker_new.py
+++ booker_new.py
@@ -1,1 +1,45 @@
 #! /usr/bin/env python
+
+import re 
+import requests 
+import pdfkit
+from collections import OrderedDict
+
+def get_page(main):
+	return requests.get(main).content
+
+def extract_url(cont):
+	
+	extr_list = re.findall('href="(.*?)"',cont)
+	return extr_list
+
+
+def make_abs_url(extr_list,main):
+
+	for a in range(len(extr_list)):
+		if not re.search("http",extr_list[a]):
+			extr_list[a] = main.rsplit('/',1)[0] + '/' + extr_list[a]
+	return extr_list
+
+
+def find_index(abs_url_list,start,end):
+
+	try:
+		st_index = abs_url_list.index(start)
+	except ValueError:
+		st_index = None
+	try:
+	    end_index = abs_url_list.index(end)
+	except ValueError:
+		end_index = None
+	return st_index,end_index
+
+def extract_ifr(cont):
+
+	extr_ifr_list = re.findall('iframe.*?src="(.*?)"',cont)
+	return extr_ifr_list
+
+
+def htmltopdf(url_list,main):
+      pdfkit.from_url(url_list,main.rsplit('/',2)[1]+".pdf")
+
